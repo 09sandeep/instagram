16c3106212b951561f2091501e5cd7b219a2cb24
Sandeep Kumar <sandeep09kumar01@gmail.com>
2022-07-20 21:37:20 +0530

    stories component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import '../styles/App.scss';
 import Navigation from './Navigation.js'; 
+import Cards from "./Cards";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      Hello World
+      <main>
+        <div className="container">
+          <Cards />
+          <Sidebar />
+        </div>
+      </main>
     </div>
   );
 }
