8967a739bec1a9bc49d2254f1b61d3242bf49b70
Sandeep Kumar <sandeep09kumar01@gmail.com>
2022-07-20 21:25:39 +0530

    2nd commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import '../styles/App.scss';
+import Navigation from './Navigation.js'; 
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
+      Hello World
     </div>
   );
 }
